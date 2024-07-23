4670fce7cce380f8692cd455179a27249e1c8dd3
Linaeformin <minsa030407@gmail.com>
2024-07-23 10:29:14 +0900

    [FEAT] CategoryDialog에서 설정한 카테고리가 WriteFragment에 반영 [FEAT] 설정한 카테고리 저장 [FEAT] CategoryDialog의 cancelBtn 처리

--- app/src/main/java/com/example/timecapsule/CategoryDialog.kt
+++ app/src/main/java/com/example/timecapsule/CategoryDialog.kt
@@ -9,7 +9,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.timecapsule.databinding.DialogWriteCategoryBinding
 
-class CategoryDialog: DialogFragment() {
+private var previousOption: Int = 0     //이전 옵션 정의
+
+class CategoryDialog : DialogFragment() {
 
     private lateinit var binding: DialogWriteCategoryBinding
 
@@ -18,12 +20,56 @@ class CategoryDialog: DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //바인딩 설정
+        // 바인딩 설정
         binding = DialogWriteCategoryBinding.inflate(inflater, container, false)
 
-        //팝업창 모서리 둥글게 만들기
+        // 팝업창 모서리 둥글게 만들기
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        //이전에 선택한 카테고리 UI 업데이트
+        updateUI(previousOption)
+
+        // radioGroup에 체인지 리스너 설정
+        binding.writeCategoryRg.setOnCheckedChangeListener { group, checkedId ->
+            previousOption = checkedId
+            when (checkedId) {
+                R.id.write_category_school_rb -> {
+                    sendSortOption("학교")    // 선택한 옵션을 부모 Fragment로 전달하는 함수
+                }
+                R.id.write_category_family_rb -> {
+                    sendSortOption("가족")    // 선택한 옵션을 부모 Fragment로 전달하는 함수
+                }
+                R.id.write_category_travel_rb -> {
+                    sendSortOption("여행")    // 선택한 옵션을 부모 Fragment로 전달하는 함수
+                }
+                R.id.write_category_friend_rb -> {
+                    sendSortOption("친구")    // 선택한 옵션을 부모 Fragment로 전달하는 함수
+                }
+                R.id.write_category_food_rb -> {
+                    sendSortOption("음식")    // 선택한 옵션을 부모 Fragment로 전달하는 함수
+                }
+            }
+        }
+
+        //취소 버튼을 클릭하면 팝업창 삭제
+        binding.dialogHomeCancelIv.setOnClickListener {
+            dismiss()
+        }
+
         return binding.root
+    }
+
+    //WriteFragment에 선택된 카테고리 전송
+    private fun sendSortOption(option: String) {
+        val result = Bundle()
+        result.putString("selectedCategory", option)
+        parentFragmentManager.setFragmentResult("categorySelection", result)
+    }
+
+    //선택된 카테고리에 따라 UI를 업데이트함
+    private fun updateUI(selectedOption: Int){
+        if(selectedOption!=0){
+            binding.writeCategoryRg.check(selectedOption)   //이전 카테고리 check
+        }
     }
 }
--- app/src/main/java/com/example/timecapsule/WriteFragment.kt
+++ app/src/main/java/com/example/timecapsule/WriteFragment.kt
@@ -15,31 +15,37 @@ class WriteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //바인딩 설정
-        binding= FragmentWriteBinding.inflate(inflater, container, false)
+        // 바인딩 설정
+        binding = FragmentWriteBinding.inflate(inflater, container, false)
 
-        //캘린더 버튼 클릭시 팝업창 출력
+        // 캘린더 버튼 클릭시 팝업창 출력
         binding.writeCalendarBtn.setOnClickListener {
             popupCalendar()
         }
 
-        //카테고리 버튼 클릭시 팝업창 출력
+        // 카테고리 버튼 클릭시 팝업창 출력
         binding.writeCategoryBtn.setOnClickListener {
             popupCategory()
+        }
+
+        //CategoryDialog에서 보낸 selectedCategory 내의 카테고리를 textView에 반영
+        parentFragmentManager.setFragmentResultListener("categorySelection", this) { _, bundle ->
+            val result = bundle.getString("selectedCategory")
+            binding.writeCategoryTv.text = result
         }
 
         return binding.root
     }
 
-    //캘린더 팝업창 출력
-    private fun popupCalendar(){
-        val dialog=CalendarDialog()
-        dialog.show(parentFragmentManager,"")
+    // 캘린더 팝업창 출력
+    private fun popupCalendar() {
+        val dialog = CalendarDialog()
+        dialog.show(parentFragmentManager, "")
     }
 
-    //카테고리 팝업창 출력
-    private fun popupCategory(){
-        val dialog=CategoryDialog()
-        dialog.show(parentFragmentManager,"")
+    // 카테고리 팝업창 출력
+    private fun popupCategory() {
+        val dialog = CategoryDialog()
+        dialog.show(parentFragmentManager, "")
     }
 }
