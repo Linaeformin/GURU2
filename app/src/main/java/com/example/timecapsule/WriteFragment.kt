c5871823afa60d5adc916a1c3a53b149eaf75f7c
Linaeformin <minsa030407@gmail.com>
2024-07-24 15:59:23 +0900

    [COMMENT] 주석 추가 [REFACTOR] 필요없는 코드 삭제

--- app/src/main/java/com/example/timecapsule/WriteFragment.kt
+++ app/src/main/java/com/example/timecapsule/WriteFragment.kt
@@ -2,13 +2,10 @@ package com.example.timecapsule
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +13,6 @@ import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.timecapsule.databinding.FragmentWriteBinding
-import java.io.ByteArrayOutputStream
 import java.time.LocalDate
 
 private var settingOption: Int=0    //설정 옵션 정의
@@ -29,7 +25,6 @@ class WriteFragment : Fragment() {
     private var day: String="25"
 
     private val galleryCode = 100
-    private val captureImage=1
     private val selectedImages=mutableListOf<Uri>()
 
     @SuppressLint("SetTextI18n")
@@ -49,6 +44,8 @@ class WriteFragment : Fragment() {
         //radioGroup에 체인지 리스너 설정
         binding.writeSettingRg.setOnCheckedChangeListener{ _, checkedId ->
             settingOption=checkedId
+
+            //체크한 옵션에 따라 textView 설정
             when(checkedId) {
                 R.id.write_setting_rb -> {
                     binding.writeCalendarBtn.visibility=View.VISIBLE
@@ -100,6 +97,7 @@ class WriteFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getCurrentDate() {
+        //오늘의 년도, 월, 일을 변수에 저장
         val currentDate= LocalDate.now()
 
         //오늘의 년도, 월, 일을 저장
@@ -112,9 +110,9 @@ class WriteFragment : Fragment() {
 
     @Suppress("DEPRECATION")
     private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK).apply {
-            type = "image/*"
-            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        val intent = Intent(Intent.ACTION_PICK).apply { //데이터 요청
+            type = "image/*"    //데이터 유형은 이미지
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)    //이미지를 하나만 선택할 수 있도록 지정
         }
         startActivityForResult(intent, galleryCode)
     }
@@ -125,34 +123,23 @@ class WriteFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == galleryCode && resultCode == Activity.RESULT_OK) {
-            data?.clipData?.let { clipData ->
-                for (i in 0 until clipData.itemCount) {
-                    val imageUri = clipData.getItemAt(i).uri
-                    addImageToLayout(imageUri)
-                    if (i == 2) break // Allow up to 5 images
-                }
-            } ?: data?.data?.let { uri ->
-                addImageToLayout(uri)
-            }
-        } else if (requestCode == captureImage && resultCode == Activity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as? Bitmap
-            imageBitmap?.let {
-                val uri = getImageUri(requireContext(), it)
+            // 사용자가 단일 이미지를 선택한 경우 처리
+            data?.data?.let { uri ->
                 addImageToLayout(uri)
             }
         }
     }
 
-    private fun addImageToLayout(uri: Uri) {
-        // 최대 이미지 개수 제한 (예: 5개)
-        if (selectedImages.size >= 5) return
 
-        // ImageView의 크기 설정 (예: 200dp x 200dp)
+    private fun addImageToLayout(uri: Uri) {
+        // 최대 이미지 개수 1개로 제한
+        if (selectedImages.size >= 1) return
+
         val imageView = binding.writeImageIv
-        val layoutParams = imageView.layoutParams as ConstraintLayout.LayoutParams
+        val layoutParams = imageView.layoutParams as ConstraintLayout.LayoutParams  //ConstraintLayout.LayoutParams로 형 변환
         layoutParams.width = dpToPx() // 200dp를 픽셀로 변환
         layoutParams.height = dpToPx() // 200dp를 픽셀로 변환
-        imageView.layoutParams = layoutParams
+        imageView.layoutParams = layoutParams   //이미지뷰에 변경된 레이아웃 파라미터 적용
 
         // ImageView에 이미지 설정
         imageView.setImageURI(uri)
@@ -163,15 +150,7 @@ class WriteFragment : Fragment() {
 
     // dp를 px로 변환하는 메서드
     private fun dpToPx(): Int {
-        val density = resources.displayMetrics.density
-        return (200 * density).toInt()
-    }
-
-    @Suppress("DEPRECATION")
-    private fun getImageUri(inContext: Context, inImage: Bitmap): Uri {
-        val bytes = ByteArrayOutputStream()
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val path = MediaStore.Images.Media.insertImage(inContext.contentResolver, inImage, "Title", null)
-        return Uri.parse(path)
+        val density = resources.displayMetrics.density  //디스플레이의 밀도 저장
+        return (200 * density).toInt()  //200dp로 설정
     }
 }
